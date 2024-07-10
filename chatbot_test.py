4a1903bc2ee1336c27a79188893bc4ddfc2b9198
Jinhui Kim <202204487@hufs.ac.kr>
2024-07-10 15:04:33 +0900

    Add box office feature

--- chatbot_test.py
+++ chatbot_test.py
@@ -3,8 +3,8 @@ from chatbot import Chat, register_call
 import os
 import wikipedia
 import python_weather
-
-
+import requests
+from datetime import datetime, timedelta
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -42,6 +42,20 @@ def who_is(session=None, query='South Korea'):
         pass
     return "I don't know about "+query
 
+@register_call("box_office")
+def call_box_office(session=None, query=None):
+    api_key = "a734e5765f4da2c8539af53bbc793e5d"  # API 키
+    target_date = (datetime.now() - timedelta(1)).strftime('%Y%m%d')
+    url = f'http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key={api_key}&targetDt={target_date}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        box_office_result = "box office ranking:\n"
+        for item in data['boxOfficeResult']['dailyBoxOfficeList']:
+            box_office_result += f"{item['rank']}. {item['movieNm']}\n"
+        return box_office_result
+    else:
+        return "Failed to retrieve box office data"
 
 first_question = "Hi, how are you?"
 chat = Chat(os.path.join(os.path.dirname(os.path.abspath(__file__)), "chatbot_test.template"))
